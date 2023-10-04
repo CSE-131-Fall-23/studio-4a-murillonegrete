45d71d2720b67cdda4b8e00fc239f5004e828f3c
andreamurillo <andreamurillo@localhost>
2023-10-04 15:35:29 -0500

    we did it

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setXscale(0,20);
+		StdDraw.setYscale(0,16);
+		
+	
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.filledRectangle(20,4,10,2);
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledRectangle(20,6,10,.25);
+		StdDraw.setPenColor(Color.red);
+		StdDraw.filledRectangle(20,8,10,2);
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledRectangle(20,10,10,.25);
+		StdDraw.setPenColor(Color.green);
+		StdDraw.filledRectangle(20,12,10,2);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -18,7 +18,38 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+		Scanner in = new Scanner(f); // making Scanner with a File
+
+		String type = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double perameterOne = in.nextDouble();
+		double perameterTwo = in.nextDouble();
+		double perameterThree = in.nextDouble();
+		double perameterFour = in.nextDouble();
 		
+
+		if (type.equals("square")) {
+			StdDraw.square(perameterOne, perameterTwo, perameterThree);
+
+		} else if (type.equals("elipse")) {
+			StdDraw.ellipse(perameterOne, perameterTwo, perameterThree, perameterFour);
+		} 
+		else {
+			double five = in.nextDouble();
+			double six = in.nextDouble();
+			double [] x  = new double [3]; 
+			x[0]= perameterOne; 
+			x[1]= perameterThree;
+			x[2] = five;
+			double [] y = new double [3];
+			y[0]=perameterTwo;
+			y[1]= perameterFour;
+			y[2]=six;
+		StdDraw.polygon(x, y);
+		
+		}
 	}
 }
